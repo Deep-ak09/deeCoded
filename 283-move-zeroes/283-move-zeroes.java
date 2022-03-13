c527e1a2cfe971776a1875322e6d9283fb35e0bf
Deep-ak09 <98847423+Deep-ak09@users.noreply.github.com>
2022-03-13 10:08:02 +0530

    Time: 2 ms (74.14%), Space: 54.9 MB (32.14%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,15 +1,21 @@
-// class Solution {
-    class Solution {
-    public void moveZeroes(int[] nums) {
-        int slow = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
-                nums[slow++] = nums[i];
-            }
+class Solution {
+public void moveZeroes(int[] nums) {
+int i=0;int j=i+1;
+        while(j<nums.length){
+                if(nums[i]==0&&nums[j]!=0){
+                        int temp=nums[i];
+                        nums[i]=nums[j];
+                        nums[j]=temp;
+                        i++;
+                }
+                else if(nums[i]!=0){
+                        i++;
+                        j++;
+                }
+                else{
+                        j++;
+                }
         }
-        for(int i = slow; i < nums.length; i++){
-            nums[i] = 0;
-        }
-    }
+   }
 }
-    // }
+
