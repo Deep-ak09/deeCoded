d14e2275cdb19dd4404f12e1d3e09bd72678e7ec
Deep-ak09 <98847423+Deep-ak09@users.noreply.github.com>
2022-04-02 20:16:14 +0530

    Time: 12 ms (36.76%), Space: 56.4 MB (13.20%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,32 +1,35 @@
 class Solution {
     public boolean validPalindrome(String s) {
-                char ch[]=s.toCharArray();
-                int l=0;
-                int r=ch.length-1;
-                while(l<=r)
-                        {
-                                if(ch[l]!=ch[r])
-                                        {
-                                        return ispal(ch,l+1,r) || ispal(ch,l,r-1);
-                                        }
-                                l++;
-                                r--;
-                        }
-                        return true;
-                }
-public boolean ispal(char[] ch,int l,int r)
-        {
-        while(l<=r)
-                {
-                if(ch[l]!=ch[r])
-                                {
-                                return false;
-                                }
-                l++;
-                r--;
-                }
-        return true;
+        return determinePalindrome(s,0);
+    }
+    /**
+     *  In this method we compare characters of {param}s from beginning and end.
+     *  Initially, diff is zero. When we face a unmatched characters we face two possibilities
+     *  either s(i,j-1) are not in palindrome or s(i+1,j) so we perform recursion again with diff+1
+     *  when ever diff > 1, program stops recursion.
+     *  If two possibilities have more than 1 differences we get false || false returning false
+     *  else if atleast one possibility is valid palindrome we get false || true => true.
+     */
+    boolean determinePalindrome(String s,int diff){
+        int i=0;
+        int j=s.length()-1;
+        int localDiff=diff;
+        while(i<=j){            
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                localDiff++;
+                if(localDiff>1)
+                    return false;
+                boolean choiceB = determinePalindrome(s.substring(i,j),localDiff);
+                boolean choiceA = determinePalindrome(s.substring(i+1,j+1),localDiff);
+                return choiceA||choiceB;
+            }   
+            i++;
+            j--;
         }
+        return true;
+    }
+    
 }
 
 
