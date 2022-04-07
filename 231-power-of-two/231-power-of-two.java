a0e447a29037b6569bcfa7f2a9ae43ca63840d79
dee-coder01 <98847423+dee-coder01@users.noreply.github.com>
2022-04-07 20:11:13 +0530

    Time: 2 ms (57.30%), Space: 41.6 MB (30.55%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-         
-        if (n <= 0)
-        return false;
-    if (n % 2 == 0)
-        return isPowerOfTwo(n / 2);
-    if (n == 1)
-        return true;
-    return false;
+            if(n<=0) return false;
+        // if (n == 1)
+        //     return true;
+        return helper(n);
+    }
+
+    public static boolean helper(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 == 1) {
+            return false;
+        }
+        return helper(n / 2);
     }
 }
